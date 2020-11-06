04766b101ed9bdea252bdb10c5549752e25a5bca
TsunMoon <kimhexi1998@gmail.com>
2020-11-06 07:33:25 +0700

    add log

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const loginFB = () => {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
+        log(user);
         // ...
       }).catch(function(error) {
         // Handle Errors here.
